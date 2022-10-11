41834f9cd3c5c6142970fdb7b720078b35b997de
Heather <heatherarmstrong@hotmail.co.uk>
2022-10-11 12:13:54 +0100

    add tests for articles without comments

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -38,24 +38,43 @@ describe('GET /api/topics', () => {
     });
 });
 
-describe('GET api/articles/:article_id', () => {
-  test('status 200, responds with the correct article object', () => {
+describe.only('GET api/articles/:article_id', () => {
+  test('status 200, responds with the correct article object and comment count', () => {
     return request(app)
-    .get("/api/articles/2")
+    .get("/api/articles/3")
     .expect(200)
     .then(({ body }) => {
       const { article } = body;
         expect(article).toEqual(
           {
-            article_id: 2,
-            title: "Sony Vaio; or, The Laptop",
-            topic: "mitch",
-            author: "icellusedkars",
-            body: "Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
-            created_at: '2020-10-16T05:03:00.000Z',
-            votes: 0,
+              article_id: 3,
+              title: "Eight pug gifs that remind me of mitch",
+              topic: "mitch",
+              author: "icellusedkars",
+              body: "some gifs",
+              created_at: "2020-11-03T09:12:00.000Z",
+              votes: 0,
+              number_of_comments: '2',
           }
         );
+    }); 
+  });
+  test('status 200, responds with the article object if there are no comments', () => {
+    return request(app)
+    .get("/api/articles/4")
+    .expect(200)
+    .then(({ body }) => {
+      const { article } = body;
+        expect(article).toEqual({
+          article_id: 4,
+          title: "Student SUES Mitch!",
+          topic: "mitch",
+          author: "rogersop",
+          body: "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
+          created_at: "2020-05-06T01:14:00.000Z",
+          votes: 0,
+          number_of_comments: '0',
+        })
     }); 
   });
   test('status 404, responds with not found when wrong article id put in', () => {
---
+++ db/data/test-data/sqlPlayground.sql
@@ -0,0 +1,9 @@
+\c nc_news_test
+
+SELECT articles.* , COUNT(comments.article_id) AS number_of_comments 
+FROM articles 
+LEFT JOIN comments ON comments.article_id = articles.article_id
+WHERE articles.article_id = 2
+GROUP BY articles.article_id;
+
+
