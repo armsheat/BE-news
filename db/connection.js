17bd919aa75129eddec6fc0457d282dd12799acd
Heather <heatherarmstrong@hotmail.co.uk>
2022-10-14 14:08:17 +0100

    remove dotenv config

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
 
-require('dotenv').config({
+/*require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
-});
+});*/
 
 const config =
   ENV === 'production'
